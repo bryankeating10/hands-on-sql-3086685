efb3bb48662330c80acffee91f63fedc70fa3d9a
Bryan Keating <158529559+bryankeating10@users.noreply.github.com>
2025-01-26 21:23:24 +0000

    Two of my own examples

--- queries/01_02/ORDERBY.sql
+++ queries/01_02/ORDERBY.sql
@@ -6,3 +6,19 @@ SELECT name 'Name', job 'Job', deptno 'Dept #',
 commission 'Commision', CONCAT('$', salary*12) 
 AS Salary FROM emp_tab 
 ORDER BY deptno DESC,commission DESC, Salary DESC;
+
+SELECT * FROM emp_tab;
+
+SELECT
+  name 'Name',
+  job 'Job',
+  deptno 'Dept #'
+FROM emp_tab
+WHERE NOT deptno=70 AND NOT name='FORD'
+AND NOT job='CLERK'
+ORDER BY name;
+
+SELECT *
+FROM emp_tab
+WHERE NOT deptno=70 AND NOT name='FORD'
+ORDER BY name;
