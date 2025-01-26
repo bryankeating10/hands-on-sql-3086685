3766690ca3b21907bcd87770f5cb581210a7e96e
Bryan Keating <158529559+bryankeating10@users.noreply.github.com>
2025-01-26 19:33:04 +0000

    Commiting changes to just ComparisonsConditions.sql, no nevermind LogicalConditions.sql

--- queries/01_02/LogicalConditions.sql
+++ queries/01_02/LogicalConditions.sql
@@ -1,6 +1,11 @@
 SELECT * FROM emp_tab 
 WHERE name LIKE 'S%' AND deptno=30;
 
+SELECT * FROM (
+    SELECT * FROM emp_tab WHERE name LIKE 'S%'
+) AS 'Test of S';
+
+
 SELECT * FROM emp_tab 
 WHERE name LIKE 'S%' OR deptno=30;
 
