8d2cb525ffbc993d71abf35861edab7793741d22
Bryan Keating <158529559+bryankeating10@users.noreply.github.com>
2025-01-26 20:08:34 +0000

    Tinkering

--- queries/01_02/OrderOfPrecedence.sql
+++ queries/01_02/OrderOfPrecedence.sql
@@ -1,9 +1,9 @@
 SELECT empno,name,deptno,salary 
 FROM emp_tab 
-WHERE deptno=30 OR deptno=40 
-AND salary>2500;
+WHERE deptno=40 OR deptno=30 
+AND salary BETWEEN 2500 AND 4900;
 
 SELECT empno,name,deptno,salary 
 FROM emp_tab 
 WHERE (deptno=30 OR deptno=40) 
-AND salary>2500;
+AND salary BETWEEN 2500 AND 4900;
