74ddaf56715d293e9893d94ad63f2990131d72b7
Bryan Keating <158529559+bryankeating10@users.noreply.github.com>
2025-01-26 20:53:12 +0000

    Ordering

--- queries/01_02/ORDERBY.sql
+++ queries/01_02/ORDERBY.sql
@@ -1,5 +1,8 @@
-SELECT * FROM emp_tab 
-WHERE deptno=30 ORDER BY salary;
+SELECT name 'Name', deptno 'Dept #',
+CONCAT('$',salary*12) 'Annual Salary' FROM emp_tab 
+WHERE deptno=30 ORDER BY salary DESC;
 
-SELECT * FROM emp_tab 
-ORDER BY deptno,salary DESC;
+SELECT name 'Name', job 'Job', deptno 'Dept #',
+commission 'Commision', CONCAT('$', salary*12) 
+AS Salary FROM emp_tab 
+ORDER BY deptno DESC,commission DESC, Salary DESC;
