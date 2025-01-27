c68f62a1ccecfd2abaf78b8f64e57bd14cc62994
Bryan Keating <158529559+bryankeating10@users.noreply.github.com>
2025-01-27 21:52:26 +0000

    Character functions

--- queries/01_03/CharacterFunctions.sql
+++ queries/01_03/CharacterFunctions.sql
@@ -11,3 +11,20 @@ SELECT INSTR('HelloWorld','e');
 SELECT TRIM('Hello World','World');
 
 SELECT REPLACE('Hello World','Hello','Good morning');
+
+SELECT 
+UPPER('Hello Bryan') AS 'Title',
+LOWER('Hello Colette') AS 'Girlfriend',
+SUBSTR('I love you',5,6) AS 'Test';
+
+SELECT
+LENGTH('Hellowork') as 'Length';
+
+SELECT
+INSTR('Bryan','n') as 'In str n of Bryan',
+LENGTH('Bryan') as 'Length of Bryan';
+
+select
+trim('Hello world', 'llo wor') as 'Trim',
+replace('I like to grill','grill','dance') as 'Dance',
+REPLACE('Hello World','Hello','Good morning') as 'World';
